fb65400711fdf2a9752ad29add61e53ccfb9f094
Heliton Bruning <heliton.bruning@gmail.com>
2020-09-20 18:02:16 -0300

    no message

--- Refactor Monolith to Microservices/udagram-api-feed/src/server.ts
+++ Refactor Monolith to Microservices/udagram-api-feed/src/server.ts
@@ -6,12 +6,11 @@ import {IndexRouter} from './controllers/v0/index.router';
 
 import bodyParser from 'body-parser';
 import {config} from './config/config';
-import {V0_FEED_MODELS, V0_USER_MODELS} from './controllers/v0/model.index';
+import {V0_FEED_MODELS,} from './controllers/v0/model.index';
 
 
 (async () => {
   await sequelize.addModels(V0_FEED_MODELS);
-  await sequelize.addModels(V0_USER_MODELS);
   await sequelize.sync();
 
   const app = express();
